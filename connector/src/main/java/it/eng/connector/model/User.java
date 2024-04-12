7e3795ceb02a6ccafbf3656b2c7276d7325f46e3
marest94 <stojanovic1994@yahoo.com>
2024-04-12 15:26:26 +0200

    Update repositories and services

--- connector/src/main/java/it/eng/connector/model/User.java
+++ connector/src/main/java/it/eng/connector/model/User.java
@@ -1,20 +1,17 @@
 package it.eng.connector.model;
 
-import java.io.Serial;
-import java.util.Collection;
-import java.util.List;
-
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
--- connector/src/main/java/it/eng/connector/repository/UserRepository.java
+++ connector/src/main/java/it/eng/connector/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package it.eng.connector.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import it.eng.connector.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import it.eng.connector.model.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
