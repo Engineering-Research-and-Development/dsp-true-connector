6d0452254c5e5ee816797cd7dc7fa7ce6eac7ed2
marest94 <stojanovic1994@yahoo.com>
2024-04-26 17:08:34 +0200

    Add MongoConfig and update ApplicationConnector.java and related files

---
+++ connector/src/main/java/it/eng/connector/configuration/MongoConfig.java
@@ -0,0 +1,17 @@
+package it.eng.connector.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = {"it.eng.connector.repository", "it.eng.catalog.repository", "it.eng.negotiation.repository"})
+public class MongoConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
