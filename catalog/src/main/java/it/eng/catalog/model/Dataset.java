1b4463729fe2089b9c19ef66d47eae4fc41022f0
marest94 <stojanovic1994@yahoo.com>
2024-04-26 17:09:16 +0200

    Add @DbRef in model classes

--- catalog/src/main/java/it/eng/catalog/model/Dataset.java
+++ catalog/src/main/java/it/eng/catalog/model/Dataset.java
@@ -1,10 +1,7 @@
 package it.eng.catalog.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import it.eng.tools.model.DSpaceConstants;
@@ -15,6 +12,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 import java.util.Set;
@@ -23,145 +24,177 @@ import java.util.stream.Collectors;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonDeserialize(builder = Dataset.Builder.class)
-@JsonPropertyOrder(value = { DSpaceConstants.CONTEXT, DSpaceConstants.ID, DSpaceConstants.TYPE, DSpaceConstants.DCT_TITLE,
-		DSpaceConstants.DCT_DESCRIPTION, DSpaceConstants.DCAT_KEYWORD, DSpaceConstants.ODRL_HAS_POLICY, 
-		DSpaceConstants.DCAT_DISTRIBUTION}, alphabetic = true)
+@JsonPropertyOrder(value = {DSpaceConstants.CONTEXT, DSpaceConstants.ID, DSpaceConstants.TYPE, DSpaceConstants.DCT_TITLE,
+        DSpaceConstants.DCT_DESCRIPTION, DSpaceConstants.DCAT_KEYWORD, DSpaceConstants.ODRL_HAS_POLICY,
+        DSpaceConstants.DCAT_DISTRIBUTION}, alphabetic = true)
+@Document(collection = "datasets")
 public class Dataset extends AbstractCatalogObject {
 
-	@JsonProperty(DSpaceConstants.ID)
-	private String id;
-	
-	// Resource
-	@JsonProperty(DSpaceConstants.DCAT_KEYWORD)
-	private List<String> keyword;
-	@JsonProperty(DSpaceConstants.DCAT_THEME)
-	private List<String> theme;
-	@JsonProperty(DSpaceConstants.DCT_CONFORMSTO)
-	private String conformsTo;
-	@JsonProperty(DSpaceConstants.DCT_CREATOR)
-	private String creator;
-	@JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
-	private List<Multilanguage> description;
-	@JsonProperty(DSpaceConstants.DCT_IDENTIFIER)
-	private String identifier;
-	@JsonProperty(DSpaceConstants.DCT_ISSUED)
-	private String issued;
-	@JsonProperty(DSpaceConstants.DCT_MODIFIED)
-	private String modified;
-	@JsonProperty(DSpaceConstants.DCT_TITLE)
-	private String title;
+    @Id
+    @JsonProperty(DSpaceConstants.ID)
+    private String id;
 
-	@NotNull
-	@JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
-	private List<Offer> hasPolicy;
-	@JsonProperty(DSpaceConstants.DCAT_DISTRIBUTION)
-	private List<Distribution> distribution;
+    // Resource
+    @JsonProperty(DSpaceConstants.DCAT_KEYWORD)
+    private List<String> keyword;
+    @JsonProperty(DSpaceConstants.DCAT_THEME)
+    private List<String> theme;
+    @JsonProperty(DSpaceConstants.DCT_CONFORMSTO)
+    private String conformsTo;
+    @JsonProperty(DSpaceConstants.DCT_CREATOR)
+    private String creator;
+    @JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
+    private List<Multilanguage> description;
+    @JsonProperty(DSpaceConstants.DCT_IDENTIFIER)
+    private String identifier;
+    @JsonProperty(DSpaceConstants.DCT_ISSUED)
+    @CreatedDate
+    private String issued;
+    @JsonProperty(DSpaceConstants.DCT_MODIFIED)
+    @LastModifiedDate
+    private String modified;
+    @JsonProperty(DSpaceConstants.DCT_TITLE)
+    private String title;
+    @NotNull
+    @JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
+    private List<Offer> hasPolicy;
+    @JsonProperty(DSpaceConstants.DCAT_DISTRIBUTION)
+    @DBRef
+    private List<Distribution> distribution;
+    @JsonIgnore
+    @CreatedBy
+    private String createdBy;
+    @JsonIgnore
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @JsonIgnore
+    @Version
+    @Field("version")
+    private Long version;
 
-	@JsonPOJOBuilder(withPrefix = "")
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Builder {
-		private final Dataset dataset;
+    @JsonPOJOBuilder(withPrefix = "")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Builder {
+        private final Dataset dataset;
 
-		private Builder() {
-			dataset = new Dataset();
-		}
+        private Builder() {
+            dataset = new Dataset();
+        }
 
-		@JsonCreator
-		public static Builder newInstance() {
-			return new Builder();
-		}
-		
-		@JsonProperty(DSpaceConstants.ID)
-		public Builder id(String id) {
-			dataset.id = id;
-			return this;
-		}
+        @JsonCreator
+        public static Builder newInstance() {
+            return new Builder();
+        }
 
-		@JsonProperty(DSpaceConstants.DCAT_KEYWORD)
-		public Builder keyword(List<String> keyword) {
-			dataset.keyword = keyword;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.ID)
+        public Builder id(String id) {
+            dataset.id = id;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCAT_THEME)
-		public Builder theme(List<String> theme) {
-			dataset.theme = theme;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCAT_KEYWORD)
+        public Builder keyword(List<String> keyword) {
+            dataset.keyword = keyword;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_CONFORMSTO)
-		public Builder conformsTo(String conformsTo) {
-			dataset.conformsTo = conformsTo;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCAT_THEME)
+        public Builder theme(List<String> theme) {
+            dataset.theme = theme;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_CREATOR)
-		public Builder creator(String creator) {
-			dataset.creator = creator;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_CONFORMSTO)
+        public Builder conformsTo(String conformsTo) {
+            dataset.conformsTo = conformsTo;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
-		public Builder description(List<Multilanguage> description) {
-			dataset.description = description;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_CREATOR)
+        public Builder creator(String creator) {
+            dataset.creator = creator;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_IDENTIFIER)
-		public Builder identifier(String identifier) {
-			dataset.identifier = identifier;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
+        public Builder description(List<Multilanguage> description) {
+            dataset.description = description;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_ISSUED)
-		public Builder issued(String issued) {
-			dataset.issued = issued;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_IDENTIFIER)
+        public Builder identifier(String identifier) {
+            dataset.identifier = identifier;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_MODIFIED)
-		public Builder modified(String modified) {
-			dataset.modified = modified;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_ISSUED)
+        public Builder issued(String issued) {
+            dataset.issued = issued;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_TITLE)
-		public Builder title(String title) {
-			dataset.title = title;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_MODIFIED)
+        public Builder modified(String modified) {
+            dataset.modified = modified;
+            return this;
+        }
 
-		@JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
-		public Builder hasPolicy(List<Offer> policies) {
-			dataset.hasPolicy = policies;
-			return this;
-		}
-		
-		@JsonProperty(DSpaceConstants.DCAT_DISTRIBUTION)
-		public Builder distribution(List<Distribution> distribution) {
-			dataset.distribution = distribution;
-			return this;
-		}
+        @JsonProperty(DSpaceConstants.DCT_TITLE)
+        public Builder title(String title) {
+            dataset.title = title;
+            return this;
+        }
 
-		public Dataset build() {
-			if(dataset.id == null) {
-				dataset.id = dataset.createNewId();
-			}
-			Set<ConstraintViolation<Dataset>> violations 
-				= Validation.buildDefaultValidatorFactory().getValidator().validate(dataset);
-			if(violations.isEmpty()) {
-				return dataset;
-			}
-			throw new ValidationException(
-					violations
-						.stream()
-						.map(v -> v.getPropertyPath() + " " + v.getMessage())
-						.collect(Collectors.joining(",")));
-			}
-	}
+        @JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
+        public Builder hasPolicy(List<Offer> policies) {
+            dataset.hasPolicy = policies;
+            return this;
+        }
 
-	@JsonProperty(value = DSpaceConstants.TYPE, access = Access.READ_ONLY)
-	public String getType() {
-		return DSpaceConstants.DCAT + Dataset.class.getSimpleName();
-	}
+        @JsonProperty(DSpaceConstants.DCAT_DISTRIBUTION)
+        public Builder distribution(List<Distribution> distribution) {
+            dataset.distribution = distribution;
+            return this;
+        }
+
+        @JsonProperty("createdBy")
+        public Builder createdBy(String createdBy) {
+            dataset.createdBy = createdBy;
+            return this;
+        }
+
+        @JsonProperty("lastModifiedBy")
+        public Builder lastModifiedBy(String lastModifiedBy) {
+            dataset.lastModifiedBy = lastModifiedBy;
+            return this;
+        }
+
+        @JsonProperty("version")
+        public Builder version(Long version) {
+            dataset.version = version;
+            return this;
+        }
+
+        public Dataset build() {
+            if (dataset.id == null) {
+                dataset.id = dataset.createNewId();
+            }
+            Set<ConstraintViolation<Dataset>> violations
+                    = Validation.buildDefaultValidatorFactory().getValidator().validate(dataset);
+            if (violations.isEmpty()) {
+                return dataset;
+            }
+            throw new ValidationException(
+                    violations
+                            .stream()
+                            .map(v -> v.getPropertyPath() + " " + v.getMessage())
+                            .collect(Collectors.joining(",")));
+        }
+    }
+
+    @JsonProperty(value = DSpaceConstants.TYPE, access = Access.READ_ONLY)
+    public String getType() {
+        return DSpaceConstants.DCAT + Dataset.class.getSimpleName();
+    }
 }
--- catalog/src/main/java/it/eng/catalog/model/Distribution.java
+++ catalog/src/main/java/it/eng/catalog/model/Distribution.java
@@ -1,9 +1,5 @@
 package it.eng.catalog.model;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-
 import it.eng.tools.model.DSpaceConstants;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -20,104 +15,122 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonDeserialize(builder = Distribution.Builder.class)
-@JsonPropertyOrder(value = { DSpaceConstants.TYPE, DSpaceConstants.DCT_FORMAT, DSpaceConstants.DCAT_ACCESS_SERVICE}
-, alphabetic = true)
+@JsonPropertyOrder(value = {DSpaceConstants.TYPE, DSpaceConstants.DCT_FORMAT, DSpaceConstants.DCAT_ACCESS_SERVICE}
+        , alphabetic = true)
+@Document(collection = "distributions")
 public class Distribution {
 
-	@JsonProperty(DSpaceConstants.DCT_TITLE)
-	private String title;
-	@JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
-	private List<Multilanguage> description;
-	@JsonProperty(DSpaceConstants.DCT_ISSUED)
-	private String issued;
-	@JsonProperty(DSpaceConstants.DCT_MODIFIED)
-	private String modified;
-	@JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
-	private List<Offer> hasPolicy;
-	
+
+    @Id
+    @JsonProperty(DSpaceConstants.ID)
+    private String id;
+
+    @JsonProperty(DSpaceConstants.DCT_TITLE)
+    private String title;
+    @JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
+    private List<Multilanguage> description;
+    @JsonProperty(DSpaceConstants.DCT_ISSUED)
+    @CreatedDate
+    private String issued;
+    @JsonProperty(DSpaceConstants.DCT_MODIFIED)
+    @LastModifiedDate
+    private String modified;
+    @JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
+    private List<Offer> hasPolicy;
+
 //	@JsonProperty(DSpaceConstants.DCT_FORMAT)
 //	private Reference format;
-	
-	@NotNull
-	@JsonProperty(DSpaceConstants.DCAT_ACCESS_SERVICE)
-	private List<DataService> accessService;
 
-	@JsonPOJOBuilder(withPrefix = "")
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Builder {
-		private final Distribution distribution;
+    @NotNull
+    @DBRef
+    @JsonProperty(DSpaceConstants.DCAT_ACCESS_SERVICE)
+    private List<DataService> accessService;
 
-		private Builder() {
-			distribution = new Distribution();
-		}
+    @JsonPOJOBuilder(withPrefix = "")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Builder {
+        private final Distribution distribution;
 
-		@JsonCreator
-		public static Builder newInstance() {
-			return new Builder();
-		}
-		
-		@JsonProperty(DSpaceConstants.DCT_TITLE)
-		public Builder title(String title) {
-			distribution.title = title;
-			return this;
-		}
+        private Builder() {
+            distribution = new Distribution();
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
-		public Builder description(List<Multilanguage> description) {
-			distribution.description = description;
-			return this;
-		}
-		
-		@JsonProperty(DSpaceConstants.DCT_ISSUED)
-		public Builder issued(String issued) {
-			distribution.issued = issued;
-			return this;
-		}
+        @JsonCreator
+        public static Builder newInstance() {
+            return new Builder();
+        }
 
-		@JsonProperty(DSpaceConstants.DCT_MODIFIED)
-		public Builder modified(String modified) {
-			distribution.modified = modified;
-			return this;
-		}
-		
-		@JsonProperty(DSpaceConstants.DCAT_ACCESS_SERVICE)
-		public Builder accessService(List<DataService> dataService) {
-			distribution.accessService = dataService;
-			return this;
-		}
-		
+        @JsonProperty(DSpaceConstants.DCT_TITLE)
+        public Builder title(String title) {
+            distribution.title = title;
+            return this;
+        }
+
+        @JsonProperty(DSpaceConstants.DCT_DESCRIPTION)
+        public Builder description(List<Multilanguage> description) {
+            distribution.description = description;
+            return this;
+        }
+
+        @JsonProperty(DSpaceConstants.DCT_ISSUED)
+        public Builder issued(String issued) {
+            distribution.issued = issued;
+            return this;
+        }
+
+        @JsonProperty(DSpaceConstants.DCT_MODIFIED)
+        public Builder modified(String modified) {
+            distribution.modified = modified;
+            return this;
+        }
+
+        @JsonProperty(DSpaceConstants.DCAT_ACCESS_SERVICE)
+        public Builder accessService(List<DataService> dataService) {
+            distribution.accessService = dataService;
+            return this;
+        }
+
 //		@JsonProperty(DSpaceConstants.DCT_FORMAT)
 //		public Builder format(Reference format) {
 //			distribution.format = format;
 //			return this;
 //		}
-		
-		@JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
-		public Builder hasPolicy(List<Offer> policies) {
-			distribution.hasPolicy = policies;
-			return this;
-		}
-		
-		public Distribution build() {
-			Set<ConstraintViolation<Distribution>> violations 
-				= Validation.buildDefaultValidatorFactory().getValidator().validate(distribution);
-			if(violations.isEmpty()) {
-				return distribution;
-			}
-			throw new ValidationException("Distribution - " +
-					violations
-						.stream()
-						.map(v -> v.getPropertyPath() + " " + v.getMessage())
-						.collect(Collectors.joining(",")));
-			}
-	}
-	
-	@JsonProperty(value = DSpaceConstants.TYPE, access = Access.READ_ONLY)
-	public String getType() {
-		return DSpaceConstants.DSPACE + Distribution.class.getSimpleName();
-	}
+
+        @JsonProperty(DSpaceConstants.ODRL_HAS_POLICY)
+        public Builder hasPolicy(List<Offer> policies) {
+            distribution.hasPolicy = policies;
+            return this;
+        }
+
+        public Distribution build() {
+            Set<ConstraintViolation<Distribution>> violations
+                    = Validation.buildDefaultValidatorFactory().getValidator().validate(distribution);
+            if (violations.isEmpty()) {
+                return distribution;
+            }
+            throw new ValidationException("Distribution - " +
+                    violations
+                            .stream()
+                            .map(v -> v.getPropertyPath() + " " + v.getMessage())
+                            .collect(Collectors.joining(",")));
+        }
+    }
+
+    @JsonProperty(value = DSpaceConstants.TYPE, access = Access.READ_ONLY)
+    public String getType() {
+        return DSpaceConstants.DSPACE + Distribution.class.getSimpleName();
+    }
 }
