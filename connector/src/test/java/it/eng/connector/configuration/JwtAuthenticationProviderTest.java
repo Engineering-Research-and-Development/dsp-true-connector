82cbf51b54d3bed979059d478a53a6786121e094
gianluca-isgro <gianluca.isgro@eng.it>
2024-07-31 14:52:36 +0200

    Modify to manage change in DapsService

--- connector/src/test/java/it/eng/connector/configuration/JwtAuthenticationProviderTest.java
+++ connector/src/test/java/it/eng/connector/configuration/JwtAuthenticationProviderTest.java
@@ -15,31 +15,33 @@ import it.eng.tools.daps.DapsService;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationProviderTest {
-	
+
 	private final static String PRINCIPAL = "principal";
 
 	@InjectMocks
 	private JwtAuthenticationProvider jwtAuthenticationProvider;
-	
+
 	@Mock
 	private DapsService dapsService;
 	@Mock
 	private JwtAuthenticationToken authentication;
-	
+
 	@Test
 	public void authenticateSuccess() {
 		when(authentication.getPrincipal()).thenReturn(PRINCIPAL);
+		when(dapsService.isDapsEnabled()).thenReturn(true);
 		when(dapsService.validateToken(PRINCIPAL)).thenReturn(true);
 		JwtAuthenticationToken jwtAuthentication = (JwtAuthenticationToken) jwtAuthenticationProvider.authenticate(authentication);
-		
+
 		assertTrue(jwtAuthentication.isAuthenticated());
 	}
-	
+
 	@Test
 	public void authenticateFailed() {
 		when(authentication.getPrincipal()).thenReturn(PRINCIPAL);
+		when(dapsService.isDapsEnabled()).thenReturn(true);
 		when(dapsService.validateToken(PRINCIPAL)).thenReturn(false);
-		
+
 		assertThrows(BadCredentialsException.class, () -> {
 			jwtAuthenticationProvider.authenticate(authentication);
 		});
