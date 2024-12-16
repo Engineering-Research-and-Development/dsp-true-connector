c62d16d9b3cc91d5cb5fff532b14b4135654398c
David Jovanovic <david.jovanovic@eng.it>
2024-12-16 15:34:29 +0100

    fixed rename

--- catalog/src/test/java/it/eng/catalog/service/DatasetServiceTest.java
+++ catalog/src/test/java/it/eng/catalog/service/DatasetServiceTest.java
@@ -113,7 +113,7 @@ public class DatasetServiceTest {
 
         String result = datasetService.getArtifactIdFromDataset(MockObjectUtil.DATASET_WITH_FILE_ID.getId());
 
-        assertEquals(MockObjectUtil.DATASET_WITH_FILE_ID.getArtifactId(), result);
+        assertEquals(MockObjectUtil.DATASET_WITH_FILE_ID.getArtifact(), result);
         verify(repository).findById(MockObjectUtil.DATASET_WITH_FILE_ID.getId());
     }
 
@@ -122,7 +122,7 @@ public class DatasetServiceTest {
     public void getArtifactIdFromDataset_notFound() {
     	Dataset mockDataset = mock(Dataset.class);
         when(repository.findById(MockObjectUtil.DATASET.getId())).thenReturn(Optional.of(mockDataset));
-        when(mockDataset.getArtifactId()).thenReturn(null);
+        when(mockDataset.getArtifact()).thenReturn(null);
 
         assertThrows(ResourceNotFoundAPIException.class, () -> datasetService.getArtifactIdFromDataset(MockObjectUtil.DATASET.getId()));
 
