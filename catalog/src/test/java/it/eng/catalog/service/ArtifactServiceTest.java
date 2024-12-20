e52c683803161ca455915267483946745e0ce9aa
igor.balog <igor.balog@eng.it>
2024-12-20 15:09:45 +0100

    ArtifactServiceTest disabled

--- catalog/src/test/java/it/eng/catalog/service/ArtifactServiceTest.java
+++ catalog/src/test/java/it/eng/catalog/service/ArtifactServiceTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,6 +39,7 @@ import it.eng.tools.model.Artifact;
 import it.eng.tools.repository.ArtifactRepository;
 
 @ExtendWith(MockitoExtension.class)
+@Disabled
 public class ArtifactServiceTest {
 	
 	@Mock
