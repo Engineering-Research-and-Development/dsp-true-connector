35c54c4f3135c20efdf21e3a0f928b148bf1cc4b
igor.balog <igor.balog@eng.it>
2024-12-20 15:02:32 +0100

    Added junit tests

--- catalog/src/test/java/it/eng/catalog/rest/api/ArtifactAPIControllerTest.java
+++ catalog/src/test/java/it/eng/catalog/rest/api/ArtifactAPIControllerTest.java
@@ -1,12 +1,16 @@
 package it.eng.catalog.rest.api;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,9 +20,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import it.eng.catalog.exceptions.CatalogErrorAPIException;
 import it.eng.catalog.service.ArtifactService;
 import it.eng.catalog.util.MockObjectUtil;
-import it.eng.tools.model.Artifact;
+import it.eng.catalog.util.MockObjectUtil;
 import it.eng.tools.response.GenericApiResponse;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,27 +37,55 @@ class ArtifactAPIControllerTest {
 	private MultipartFile file;
 	@Mock
 	private ObjectId objId;
-	@Mock
-	private List<Artifact> gridFSList;
 	
 	@InjectMocks
 	private ArtifactAPIController controller;
-	/*
+	
 	@Test
-	void testUploadFile() {
-		when(artifactService.storeFile(file, MockObjectUtil.DATASET.getId())).thenReturn(objId);
-		when(objId.toHexString()).thenReturn("StoredFile");
-		ResponseEntity<String> response = controller.uploadFile(file, MockObjectUtil.DATASET.getId());
-		assertNotNull(response);
-		assertTrue(HttpStatus.OK.equals(response.getStatusCode()));	
+	@DisplayName("Upload - success")
+	public void testUpload() {
+		when(artifactService.uploadArtifact(file, MockObjectUtil.DATASET.getId(), null)).thenReturn(MockObjectUtil.ARTIFACT_FILE);
+		
+		ResponseEntity<GenericApiResponse<JsonNode>> response = controller.uploadArtifact(file, null, MockObjectUtil.DATASET.getId());
+		
+		assertTrue(StringUtils.isNotBlank(response.getBody().getData().toString()));
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+	
+	@Test
+	@DisplayName("Upload - fail")
+	public void testUpload_failed() {
+		when(artifactService.uploadArtifact(file, MockObjectUtil.DATASET.getId(), null)).thenThrow(CatalogErrorAPIException.class);
+		
+		assertThrows(CatalogErrorAPIException.class, ()-> controller.uploadArtifact(file, null, MockObjectUtil.DATASET.getId()));
+		
 	}
 
 	@Test
-	void testListArtifacts() {
-		when(artifactService.listArtifacts(null)).thenReturn(gridFSList);
-		ResponseEntity<GenericApiResponse<List<Artifact>>> response = controller.listArtifacts(null);
-		assertNotNull(response);
-		assertTrue(HttpStatus.OK.equals(response.getStatusCode()));
+	@DisplayName("Get all artifacts - success")
+	public void testListArtifacts() {
+		when(artifactService.getArtifacts(null))
+		.thenReturn(List.of(MockObjectUtil.ARTIFACT_FILE, MockObjectUtil.ARTIFACT_EXTERNAL));
+		
+		ResponseEntity<GenericApiResponse<JsonNode>> response = controller.getArtifacts(null);
+		
+		
+		assertTrue(response.getBody().getData().has(1));
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
-*/
+	
+	@Test
+	@DisplayName("Get artifact by id - success")
+	public void testGetArtifact() {
+		when(artifactService.getArtifacts(MockObjectUtil.ARTIFACT_FILE.getId()))
+		.thenReturn(List.of(MockObjectUtil.ARTIFACT_FILE));
+		
+		ResponseEntity<GenericApiResponse<JsonNode>> response = controller.getArtifacts(MockObjectUtil.ARTIFACT_FILE.getId());
+		
+		
+		assertTrue(response.getBody().getData().has(0));
+		assertFalse(response.getBody().getData().has(1));
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
 }
