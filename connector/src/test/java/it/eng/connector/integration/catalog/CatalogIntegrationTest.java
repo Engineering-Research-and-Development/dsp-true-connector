da9cfe928acc1ba2029f040ec9442a015e6886ae
davidjovanovic <66942966+davidjovanovic@users.noreply.github.com>
2025-06-19 15:27:30 +0200

    Hotfix/file still uploading (#171)
    ### Added
    
    - added try-catch block to handle exceptions in DataTransferApiService, when creating DataAddress with presigned URL
    - On Catalog request, filter out datasets serving files still uploading

--- connector/src/test/java/it/eng/connector/integration/catalog/CatalogIntegrationTest.java
+++ connector/src/test/java/it/eng/connector/integration/catalog/CatalogIntegrationTest.java
@@ -1,22 +1,5 @@
 package it.eng.connector.integration.catalog;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Collections;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.web.servlet.ResultActions;
-
 import it.eng.catalog.model.Catalog;
 import it.eng.catalog.model.CatalogError;
 import it.eng.catalog.model.Dataset;
@@ -26,13 +9,43 @@ import it.eng.catalog.serializer.CatalogSerializer;
 import it.eng.catalog.util.CatalogMockObjectUtil;
 import it.eng.connector.integration.BaseIntegrationTest;
 import it.eng.connector.util.TestUtil;
+import it.eng.tools.s3.properties.S3Properties;
+import it.eng.tools.s3.service.S3ClientService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CatalogIntegrationTest extends BaseIntegrationTest {
 
 	@Autowired
 	private CatalogRepository catalogRepository;
+
 	@Autowired
 	private DatasetRepository datasetRepository;
+
+	@Autowired
+	private S3ClientService s3ClientService;
+
+	@Autowired
+	private S3Properties s3Properties;
+
 	private Catalog catalog;
 	private Dataset dataset;
 	
@@ -54,13 +67,37 @@ public class CatalogIntegrationTest extends BaseIntegrationTest {
 		datasetRepository.deleteAll();
 		catalogRepository.deleteAll();
 	}
+
     @Test
     @DisplayName("Get catalog - success")
     @WithUserDetails(TestUtil.CONNECTOR_USER)
     public void getCatalogSuccessfulTest() throws Exception {
-		
+
+		String fileContent = "Hello, World!";
+
+		MockMultipartFile file
+				= new MockMultipartFile(
+				"file",
+				"hello.txt",
+				MediaType.TEXT_PLAIN_VALUE,
+				fileContent.getBytes()
+		);
+
+		ContentDisposition contentDisposition = ContentDisposition.attachment()
+				.filename(file.getOriginalFilename())
+				.build();
+
+		try {
+			s3ClientService.uploadFile(file.getInputStream(), s3Properties.getBucketName(), dataset.getId(),
+					file.getContentType(), contentDisposition.toString());
+		} catch (Exception e) {
+			throw new Exception("File storing aborted, " + e.getLocalizedMessage());
+		}
+
+		Thread.sleep(2000); // wait for the file to be uploaded to S3
+
     	String body = CatalogSerializer.serializeProtocol(CatalogMockObjectUtil.CATALOG_REQUEST_MESSAGE);
-    	
+
     	final ResultActions result =
     			mockMvc.perform(
     					post("/catalog/request")
@@ -68,15 +105,53 @@ public class CatalogIntegrationTest extends BaseIntegrationTest {
     					.contentType(MediaType.APPLICATION_JSON));
     	result.andExpect(status().isOk())
     		.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    	
+
       	String response = result.andReturn().getResponse().getContentAsString();
       	Catalog catalogResponse = CatalogSerializer.deserializeProtocol(response, Catalog.class);
       	assertNotNull(catalogResponse);
+		assertFalse(catalogResponse.getDataset().isEmpty());
+
+//		remove the file from S3 after the test
+		List<String> files = s3ClientService.listFiles(s3Properties.getBucketName());
+		if (files != null) {
+			for (String s3File : files) {
+				s3ClientService.deleteFile(s3Properties.getBucketName(), s3File);
+			}
+		}
     }
+
+	@Test
+	@DisplayName("Get catalog - check if datasets which files are not in S3 are removed from catalog response")
+	@WithUserDetails(TestUtil.CONNECTOR_USER)
+	public void getCatalogWithoutDatasetsThatHaveNoFilesTest() throws Exception {
+
+		String body = CatalogSerializer.serializeProtocol(CatalogMockObjectUtil.CATALOG_REQUEST_MESSAGE);
+
+		// Ensure the S3 bucket is empty before the test
+		List<String> files = s3ClientService.listFiles(s3Properties.getBucketName());
+		if (files != null) {
+			for (String file : files) {
+				s3ClientService.deleteFile(s3Properties.getBucketName(), file);
+			}
+		}
+
+		final ResultActions result =
+				mockMvc.perform(
+						post("/catalog/request")
+								.content(body)
+								.contentType(MediaType.APPLICATION_JSON));
+		result.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+		String response = result.andReturn().getResponse().getContentAsString();
+		Catalog catalogResponse = CatalogSerializer.deserializeProtocol(response, Catalog.class);
+		assertNotNull(catalogResponse);
+		assertNull(catalogResponse.getDataset());
+	}
     
     @Test
     @DisplayName("Get catalog - unauthorized")
-    public void getCatalogSUnauthorizedTest() throws Exception {
+    public void getCatalogUnauthorizedTest() throws Exception {
     	
     	String body = CatalogSerializer.serializeProtocol(CatalogMockObjectUtil.CATALOG_REQUEST_MESSAGE);
     	
