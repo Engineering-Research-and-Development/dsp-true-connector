824e0ae7a19d9dee37d5eeee9f44553783f719fd
IgorBalog_Eng <igor.balog@eng.it>
2026-07-21 15:01:42 +0200

    Implemented KeycloakAuthService
    New login keycloak properties
    Added junit test

---
+++ connector/src/test/java/it/eng/connector/service/KeycloakAuthServiceImplTest.java
@@ -0,0 +1,136 @@
+package it.eng.connector.service;
+
+import it.eng.tools.auth.keycloak.KeycloakLoginProperties;
+import it.eng.tools.client.rest.OkHttpRestClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class KeycloakAuthServiceImplTest {
+
+    @Mock
+    private KeycloakLoginProperties keycloakLoginProperties;
+    @Mock
+    private OkHttpRestClient okHttpRestClient;
+
+    @Mock
+    private Response response;
+
+    @InjectMocks
+    private KeycloakAuthServiceImpl keycloakAuthServiceImpl;
+
+    @Test
+    @DisplayName("Test login functionality")
+    void login() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getTokenUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(true);
+        when(response.body()).thenReturn(okhttp3.ResponseBody.create("{\"access_token\":\"dummy-access-token\",\"refresh_token\":\"dummy-refresh-token\",\"expires_in\":3600}",
+                okhttp3.MediaType.parse("application/json")));
+
+        AuthService.AuthTokens authTokens = keycloakAuthServiceImpl.login("admin", "admin");
+
+        assertNotNull(authTokens);
+        assertNotNull(authTokens.accessToken());
+        assertNotNull(authTokens.refreshToken());
+        assertEquals(3600, authTokens.expiresInSeconds());
+    }
+
+    @Test
+    @DisplayName("keycloak returned error during login")
+    void loginError() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getTokenUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(false);
+
+        assertThrows(BadCredentialsException.class, () -> {
+            keycloakAuthServiceImpl.login("admin", "admin");
+        });
+    }
+
+    @Test
+    @DisplayName("Test refresh functionality")
+    void refresh() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getTokenUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(true);
+        when(response.body()).thenReturn(okhttp3.ResponseBody.create("{\"access_token\":\"dummy-access-token\",\"refresh_token\":\"dummy-refresh-token\",\"expires_in\":3600}",
+                okhttp3.MediaType.parse("application/json")));
+
+        AuthService.AuthTokens authTokens = keycloakAuthServiceImpl.refresh("dummy-refresh-token");
+
+        assertNotNull(authTokens);
+        assertNotNull(authTokens.accessToken());
+        assertNotNull(authTokens.refreshToken());
+        assertEquals(3600, authTokens.expiresInSeconds());
+    }
+
+    @Test
+    @DisplayName("keycloak returned error during refresh")
+    void refreshError() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getTokenUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(false);
+        when(response.body()).thenReturn(okhttp3.ResponseBody.create("{\"error\":\"invalid_token\",\"error_description\":\"Refresh token is invalid\"}",
+                okhttp3.MediaType.parse("application/json")));
+
+        assertThrows(BadCredentialsException.class, () -> {
+            keycloakAuthServiceImpl.refresh("dummy-refresh-token");
+        });
+    }
+
+    @Test
+    @DisplayName("Logout successful")
+    void logout() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getLogoutUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/logout");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(true);
+
+        assertDoesNotThrow(() -> {
+            keycloakAuthServiceImpl.logout("dummy-refresh-token");
+        });
+    }
+
+    @Test
+    @DisplayName("Logout failed")
+    void logoutError() {
+        when(keycloakLoginProperties.getClientId()).thenReturn("my-client-id");
+        when(keycloakLoginProperties.getClientSecret()).thenReturn("my-client-secret");
+        when(keycloakLoginProperties.getLogoutUrl()).thenReturn("http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/logout");
+        when(okHttpRestClient.executeCall(any(Request.class))).thenReturn(response);
+
+        when(response.isSuccessful()).thenReturn(false);
+        when(response.body()).thenReturn(okhttp3.ResponseBody.create("{\"error\":\"invalid_token\",\"error_description\":\"Refresh token is invalid\"}",
+                okhttp3.MediaType.parse("application/json")));
+
+        assertThrows(BadCredentialsException.class, () -> {
+            keycloakAuthServiceImpl.logout("dummy-refresh-token-invalid");
+        });
+    }
+}
