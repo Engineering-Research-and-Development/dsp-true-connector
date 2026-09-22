f692f946c7d020d98a95e9b22bf26146431c4faf
IgorBalog_Eng <igor.balog@eng.it>
2026-09-22 13:08:56 +0200

    Upgraded GHA action versions
    Added missing GHA tests to integration matrix

--- dcp/dcp-issuer/src/test/java/it/eng/dcp/issuer/integration/BaseIssuerIntegrationTest.java
+++ dcp/dcp-issuer/src/test/java/it/eng/dcp/issuer/integration/BaseIssuerIntegrationTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -40,7 +40,7 @@ public class BaseIssuerIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     protected KeyService keyService;
 
     protected ECKey holderKeyPair;
--- dcp/dcp-issuer/src/test/java/it/eng/dcp/issuer/integration/IssuerControllerIT.java
+++ dcp/dcp-issuer/src/test/java/it/eng/dcp/issuer/integration/IssuerControllerIT.java
@@ -22,8 +22,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
@@ -59,10 +59,10 @@ public class IssuerControllerIT extends BaseIssuerIntegrationTest {
     @Autowired
     private BaseDidDocumentConfiguration config;
 
-    @MockBean
+    @MockitoBean
     private DidResolverService didResolverService;
 
-    @MockBean
+    @MockitoBean
     private CredentialDeliveryService credentialDeliveryService;
 
     private static final String ISSUER_DID = "did:test:issuer";
